2f40293c0f66a1b828eebb5475b44fc2ddd51385
YanaWebProgrammer <teukita15@gmail.com>
2023-09-12 16:55:33 +0700

    manipulasi array

--- sampleCode/27. array/script.js
+++ sampleCode/27. array/script.js
@@ -36,15 +36,28 @@
 // console.log(arr)
 
 //3. pop - menghapus index terakhir dalam array
-arr = ['Yana', 'Yani', 'Yuni', 'Yeni', 'Yuyun', 'nambah', 'nambah lagi'];
+var arr = ['Yana', 'Yani', 'Yuni', 'Yeni', 'Yuyun', 'nambah', 'nambah lagi'];
 // arr.pop()
 // arr.pop()
 // console.log(arr)
 
-arr.unshift('nambahdidepan', 'nambah didepandua')
+// arr.unshift('nambahdidepan', 'nambah didepandua')
 // console.log(arr)
 
-arr.shift()
 // arr.shift()
-console.log(arr)
+// arr.shift()
+// console.log(arr)
+
+//4. splice - Menyisipkan, menghapus data dengan customize posisi index
+
+// splice(indexAwal, mauDihapusBerapa, elemenBaru1, elemenBaru2)
+// arr.splice(2, 0, 'nyisipditengah');//nyisip data di index[2], tidak ada yg dihapus, index yg disisipkan
+
+// arr.splice(1, 2, 'tambahsatu', 'tambahkedua')//dimulai dari index[1] dihapus sebanyak dua data kemudian tambahkan dua index lagi
+// console.log(arr.join(' - '));
+
+//5. slice - mengambil element
+var arr2 = arr.slice(1,3)//ambil index [1] kemudian index[3] dikurangi 1 jadinya index[2], nomer belakang itu selalu dikurangi 1 jadinya yg di slice index[1], [2]
+console.log(arr.join(' - '));
+console.log(arr2.join(' - '));
 
