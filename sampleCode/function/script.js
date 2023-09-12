fa9926480a542f93330377ff16d6b43e7915ab3e
YanaWebProgrammer <teukita15@gmail.com>
2023-09-12 08:40:35 +0700

    pembuatan function

---
+++ sampleCode/function/script.js
@@ -0,0 +1,21 @@
+// // kerangka function
+// function penjumlahanDuaPersegi(satu, dua){
+
+//     var persegiSatu = satu * satu;
+//     var persegiDua = dua * dua;
+//     var total = persegiSatu + persegiDua;
+
+//     return total
+// }
+
+// alert(penjumlahanDuaPersegi(10, 10))
+
+// alert(penjumlahanDuaPersegi(2, 2))
+
+function penjumlahan(angka1, angka2){
+    hasil = angka1 + angka2;
+
+    return hasil
+}
+
+document.writeln('hasilnya : ' + penjumlahan(25, 25)) 
