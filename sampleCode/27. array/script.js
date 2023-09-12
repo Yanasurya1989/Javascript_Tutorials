03cbc1c78998fa404608e338e1ded4bef9922eb0
YanaWebProgrammer <teukita15@gmail.com>
2023-09-12 14:58:22 +0700

    array

---
+++ sampleCode/27. array/script.js
@@ -0,0 +1,50 @@
+// 1. menampilkan isi array
+// arr = ['a', 2, true]
+// console.log(arr)
+
+// 2. Mengisi array
+// arr = [];
+// arr[0] = "Yana";
+// arr[1] = 1;
+// arr[2] = true;
+// arr[5] = "Yana Surya";
+
+// console.log(arr)
+
+//3. Menghapus array
+// arr = ['a', 1, true]
+// arr [1] = undefined
+// console.log(arr)
+
+//4. Menampilkan isi array dengan looping
+// arr = ['Yana', 'Yani', 'Yuni', 'Yeni', 'Yuyun', 'nambah', 'nambah lagi'];
+
+// for(i=0; i < arr.length; i++){//length berfungsi untuk mengetahui jumlah index dalam array
+//     console.log('Mahasiswa ke-' + (i+1) + ' adalah : ' + arr[i])
+//     document.writeln('<p>Mahasiswa ke-' + (i+1) + ' adalah : ' + arr[i] + '</p>')
+// }
+
+
+// Method dalam array
+// 1. Join
+// arr = ['Yana', 'Yani', 'Yuni', 'Yeni', 'Yuyun', 'nambah', 'nambah lagi'];
+// console.log(arr.join(' - '))
+
+//2. push - Menambahkan index array
+// arr = ['Yana', 'Yani', 'Yuni', 'Yeni', 'Yuyun', 'nambah', 'nambah lagi'];
+// arr.push('satu', 'dua', 'tiga')
+// console.log(arr)
+
+//3. pop - menghapus index terakhir dalam array
+arr = ['Yana', 'Yani', 'Yuni', 'Yeni', 'Yuyun', 'nambah', 'nambah lagi'];
+// arr.pop()
+// arr.pop()
+// console.log(arr)
+
+arr.unshift('nambahdidepan', 'nambah didepandua')
+// console.log(arr)
+
+arr.shift()
+// arr.shift()
+console.log(arr)
+
