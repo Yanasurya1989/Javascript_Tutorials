20af76bf5b2ead3b4329083524de066909d6d3b4
YanaWebProgrammer <teukita15@gmail.com>
2023-09-12 13:26:11 +0700

    rekursif

---
+++ sampleCode/25. rekursif/script.js
@@ -0,0 +1,20 @@
+// contoh rekursif
+
+
+
+// function tes(n){
+//     if (n === 0) return //ini hasil shorthand, jika ditulis lengkap seperti ini
+//     // if(n===0){
+//     //     return
+//     // }
+//     console.log(n);
+//     return tes(n-1)
+// }
+
+// tes(10);
+
+
+// rekursif diatas adalah bentuk lain dari looping
+for(i=10; i >= 1; i--){
+    console.log(i)
+}
